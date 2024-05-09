ca7e7d1df2b535cb5f49219c230eb4a493d367b6
jorgeluiz <thejljorgeluiz@gmail.com>
2024-05-09 08:54:51 -0300

    fix:limitação de casas decimais

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function atualizarTabela(){
 function atualizaMediaFinal(){
     const mediaFinal = calcularMediaFinal() 
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2)
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >=notaMinima ? spanAprovado : spanReprovado
 
 }
